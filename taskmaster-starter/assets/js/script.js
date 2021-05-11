d2af357a8b8c5d690a77c36054b652162f3f07ef
LaMonica_Arnold <ltarnold03@yahoo.com>
2021-05-11 13:14:37 -0500

    add form

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -107,7 +107,7 @@ $(".list-group").on("click", "span", function () {
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text')", function() {
+$(".list-group").on("blur", "input[type='text']", function() {
   // get current text
   var date = $(this)
   .val()
